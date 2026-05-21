c325329af2ee2dc2591892bc4fc0a944c93c00f9
usari14 <udogar583@gmail.com>
2026-05-22 02:45:16 +0500

    first1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProduction ? "/shotify" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
